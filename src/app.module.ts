3a2b3acc688d71dad8b1c38ab63c5bbe79711ce4
rafaapcode <rafaap2003@gmail.com>
2023-02-09 20:28:02 -0300

    feat: clean the structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServiceService } from './service/service/service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [ServiceService],
 })
 export class AppModule {}
