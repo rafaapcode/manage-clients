9e1e757d2c5206d638f78f57ec2197cc9f482627
rafaapcode <rafaap2003@gmail.com>
2023-02-20 07:55:57 -0300

    fix: fix paths and features of controller

--- src/presentation/ClientController/client-controller.ts
+++ src/presentation/ClientController/client-controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Body, Get, Post, Put, Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { ClientCreateDto } from '../../shared/dtos/client.create.dto';
 import {
   CreateClientUseCase,
@@ -17,29 +26,30 @@ export class ClientController {
   private readonly getById = new GetByIdClientUseCase(this.db);
   private readonly patch = new PatchClientUseCase(this.db);
   private readonly put = new PutClientUseCase(this.db);
-  // constructor(
-  //   getAll: GetAllClientUseCase,
-  //   create: CreateClientUseCase,
-  //   getById: GetByIdClientUseCase,
-  //   patch: PatchClientUseCase,
-  //   put: PutClientUseCase,
-  // ) {}
 
   @Post()
   public async createClient(@Body() client: ClientCreateDto) {
     return await this.create.execute(client);
   }
 
-  @Patch(':id')
+  @Patch('update/:id')
   public async editClient(
-    @Param() id: number,
+    @Param('id', ParseIntPipe) id: number,
     @Body() client: Partial<ClientCreateDto>,
   ) {
     return await this.patch.execute(id, client);
   }
 
+  @Put(':id')
+  public async updateClient(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() client: ClientCreateDto,
+  ) {
+    return await this.put.execute(id, client);
+  }
+
   @Get(':id')
-  public async getClient(@Param() id: number) {
+  public async getClient(@Param('id', ParseIntPipe) id: number) {
     return await this.getById.execute(id);
   }
 
