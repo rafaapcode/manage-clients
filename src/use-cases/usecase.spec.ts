02f5e6f305a05e88f1db5526c63482ff86b80e6b
rafaapcode <rafaap2003@gmail.com>
2023-02-20 07:25:18 -0300

    refactor: create a method makeSut and turning the request body global

--- src/use-cases/usecase.spec.ts
+++ src/use-cases/usecase.spec.ts
@@ -10,52 +10,69 @@ import { DatabaseInMemory } from '../data/memory/DatabaseInMemory';
 
 const mockDatabase = new DatabaseInMemory();
 
+const makeSut = () => {
+  const createClient = new CreateClientUseCase(mockDatabase);
+  const getAllClient = new GetAllClientUseCase(mockDatabase);
+  const getClientById = new GetByIdClientUseCase(mockDatabase);
+  const patchClient = new PatchClientUseCase(mockDatabase);
+  const updateAllclient = new PutClientUseCase(mockDatabase);
+
+  return {
+    createClient,
+    getAllClient,
+    getClientById,
+    patchClient,
+    updateAllclient,
+  };
+};
+
+let request = {
+  name: 'Joao',
+  birthdate: '01/02/2000',
+  sex: 'male',
+  healthIssues: [{ name: 'diabetes', classification: 1 }],
+};
+
 describe('Client use cases', () => {
   beforeAll(() => {
     mockDatabase.allDb = [];
   });
 
   it('should create a new client', async () => {
-    const sut = new CreateClientUseCase(mockDatabase);
-    const request = {
-      name: 'Joao',
-      birthdate: '01/02/2000',
-      sex: 'male',
-      healthIssues: [{ name: 'diabetes', classification: 1 }],
-    };
-    const client = await sut.execute(request);
+    const { createClient } = makeSut();
+    const client = await createClient.execute(request);
     expect(client).toBeTruthy();
     expect(mockDatabase.allDb.length).toEqual(1);
   });
 
   it('should return all clients', async () => {
-    const sut = new GetAllClientUseCase(mockDatabase);
-    const allClient = await sut.execute();
+    const { getAllClient } = makeSut();
+    const allClient = await getAllClient.execute();
     expect(allClient[0].name).toBe('Joao');
     expect(allClient).toEqual(expect.arrayContaining([expect.any(Object)]));
   });
 
   it('should return a client by id', async () => {
-    const sut = new GetByIdClientUseCase(mockDatabase);
-    const client = await sut.execute(1);
+    const { getClientById } = makeSut();
+    const client = await getClientById.execute(1);
     expect(client.name).toBe('Joao');
   });
 
   it('should return a client with the field updated', async () => {
-    const sut = new PatchClientUseCase(mockDatabase);
-    const client = await sut.execute(1, { birthdate: '26/09/1997' });
+    const { patchClient } = makeSut();
+    const client = await patchClient.execute(1, { birthdate: '26/09/1997' });
     expect(client.birthdate).toBe('26/09/1997');
   });
 
   it('should return a client updated', async () => {
-    const sut = new PutClientUseCase(mockDatabase);
-    const request = {
+    const { updateAllclient } = makeSut();
+    request = {
       name: 'Gabriel Nunes',
       birthdate: '20/12/2015',
       sex: 'male',
       healthIssues: [{ name: 'Preguiça', classification: 2 }],
     };
-    const client = await sut.execute(1, request);
+    const client = await updateAllclient.execute(1, request);
     expect(client).toEqual(request);
   });
 });
